1e702edd21bfb29634c076a8eb132c2dc14dcf28
suman03-en <sfuyal59@gmail.com>
2025-09-12 12:26:28 +0545

    Post_list and details views and templates added

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import Http404
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    posts = Post.published.all()
+    return render(
+        request,
+        'blog/post/list.html',
+        {'posts':posts}
+                )
+
+def post_detail(request,id):
+    try:
+        post = Post.published.get(id=id)
+    except Post.DoesNotExist:
+        raise Http404("No post found.")
+    return render(
+        request,
+        'blog/post/detail.html',
+        {'post':post}
+    )
